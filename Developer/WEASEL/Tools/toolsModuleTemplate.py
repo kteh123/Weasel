b0f0d324781b1a6e0cd7e7b8123ef7034e5e4e09
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2020-07-27 19:39:09 +0100

    updated toolsModuleTemplate with saveSeries

--- Developer/WEASEL/Tools/toolsModuleTemplate.py
+++ Developer/WEASEL/Tools/toolsModuleTemplate.py
@@ -30,7 +30,7 @@ def returnPixelArray(imagePath, func):
         else:
             return None
     except Exception as e:
-            print('Error in function squareDICOM_Image.returnPixelArray: ' + str(e))
+            print('Error in function #.returnPixelArray: ' + str(e))
 
 #This function name must not be changed
 def saveImage(objWeasel):
@@ -105,3 +105,87 @@ def showSavingResultsMessageBox(objWeasel):
         "<H4>Saving results into a new DICOM Series</H4>",
         "Processing DICOM images")
     
+####################################################################################
+# Steve, please have a look at the functions below and ask me any questions you have
+# None of this was tested yet, still need some time to perfect this.
+
+# This method is not going to be used yet, 
+# but please notice the use of readDICOM_Image.returnSeriesPixelArray(imagePathList)
+def returnPixelArray(imagePathList, func):
+    """Applies the algorithm(s) in the function, func to
+    an image and returns the resulting PixelArray"""
+    try:
+        if os.path.exists(imagePathList[0]):
+            pixelArray = readDICOM_Image.returnSeriesPixelArray(imagePathList)
+            derivedImage = func(pixelArray)
+            return derivedImage
+        else:
+            return None
+    except Exception as e:
+            print('Error in function #.returnPixelArray: ' + str(e))
+
+
+def saveSeries(objWeasel):
+    """Creates a subwindow that displays either a series of DICOM images
+    processed using the algorithm(s) in funcAlgorithm."""
+    try:
+        imagePathList, studyID = returnImagePathList(objWeasel)
+        
+		messageWindow.displayMessageSubWindow(objWeasel,
+            "<H4>Calculating <Insert message here></H4>",
+            "Calculating Template Parametric Map")
+        messageWindow.setMsgWindowProgBarMaxValue(objWeasel,3)
+        messageWindow.setMsgWindowProgBarValue(objWeasel,1)
+		
+		# Get resulting array. Not tested yet, hence it's commented
+		# pixelArray = returnPixelArray(imagePathList, funcAlgorithm)
+		# Steve, in order to make it work you can use the line below for now
+		pixelArray = readDICOM_Image.returnSeriesPixelArray(imagePathList)
+		
+		# Suggestion for error management
+		# For eg., the script could "conclude" that the algorithm is not applicable
+		if isinstance(pixelArray, str):
+			messageWindow.displayMessageSubWindow(objWeasel, pixelArray)
+            raise Exception(pixelArray)
+		
+		if hasattr(readDICOM_Image.getDicomDataset(imagePathList[0]), 'PerFrameFunctionalGroupsSequence'):
+            # If it's Enhanced MRI
+            numImages = 1
+            derivedImageList = [pixelArray]
+            derivedImageFilePath = saveDICOM_Image.returnFilePath(imagePathList[0], FILE_SUFFIX)
+            derivedImagePathList = [derivedImageFilePath]
+        else:
+            # Iterate through list of images (slices) and save the resulting Map for each DICOM image
+            derivedImagePathList = []
+            derivedImageList = []
+            numImages = (1 if len(np.shape(derivedImage)) < 3 else np.shape(derivedImage)[0])
+            for index in range(numImages):
+				#Joao Sousa needs to review the use of imagePathList in the following line
+                derivedImageFilePath = saveDICOM_Image.returnFilePath([index], FILE_SUFFIX)
+                derivedImagePathList.append(derivedImageFilePath)
+                if numImages==1:
+                    derivedImageList.append(derivedImage)imagePathList
+                else:
+                    derivedImageList.append(derivedImage[index, ...])
+
+        messageWindow.displayMessageSubWindow(objWeasel, 
+            "<H4>Saving {} DICOM files for <Insert message here></H4>".format(numImages),
+            "Saving Template Parametric Map")
+        messageWindow.setMsgWindowProgBarMaxValue(objWeasel,3)
+        messageWindow.setMsgWindowProgBarValue(objWeasel,2)
+
+        # Save new DICOM series locally
+        saveDICOM_Image.saveDicomNewSeries(
+            derivedImagePathList, imagePathList, derivedImageList, FILE_SUFFIX)
+		#Joao Sousa needs to review the use of imagePathList in the following line
+        newSeriesID = interfaceDICOMXMLFile.insertNewSeriesInXMLFile(objWeasel,
+                        imagePathList[:len(derivedImagePathList)],
+                        derivedImagePathList, FILE_SUFFIX)
+        messageWindow.setMsgWindowProgBarValue(objWeasel,3)                                              
+        messageWindow.closeMessageSubWindow(objWeasel)
+        displayImageColour.displayMultiImageSubWindow(objWeasel,
+            derivedImagePathList, studyID, newSeriesID)
+        treeView.refreshDICOMStudiesTreeView(objWeasel, newSeriesID)
+			
+    except Exception as e:
+        print('Error in #.saveSeries: ' + str(e))
