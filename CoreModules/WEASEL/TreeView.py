b70057b7039cb3b8016a628ed8286243af860024
S Shillitoe <s.shillitoe@sheffield.ac.uk>
2020-09-07 22:26:06 +0100

    WIP - Adding Subject to Tree View

--- CoreModules/WEASEL/TreeView.py
+++ CoreModules/WEASEL/TreeView.py
@@ -11,6 +11,23 @@ import time
 import CoreModules.WEASEL.Menus  as menus
 logger = logging.getLogger(__name__)
 
+
+def createTreeBranch(self, branchName, branch, parent, treeWidgetItemCounter, branchList = None):
+    branchID = branch.attrib['id']
+    thisBranch = QTreeWidgetItem(parent)
+    if type(branchList) == list:
+        branchList.append(thisBranch)
+    treeWidgetItemCounter += 1
+    self.progBar.setValue(treeWidgetItemCounter)
+    thisBranch.setText(0, branchName + " - {}".format(branchID))
+    thisBranch.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+    #put a checkbox in front of this branch
+    thisBranch.setFlags(thisBranch.flags() | Qt.ItemIsUserCheckable)
+    thisBranch.setCheckState(0, Qt.Unchecked)
+    thisBranch.setExpanded(True)
+    return thisBranch, treeWidgetItemCounter
+
+
 def makeDICOMStudiesTreeView(self, XML_File_Path):
         """Uses an XML file that describes a DICOM file structure to build a
         tree view showing a visual representation of that file structure."""
@@ -19,14 +36,9 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
             if os.path.exists(XML_File_Path):
                 self.DICOM_XML_FilePath = XML_File_Path
                 self.DICOMfolderPath, _ = os.path.split(XML_File_Path)
-                start_time=time.time()
                 self.objXMLReader.parseXMLFile(
                     self.DICOM_XML_FilePath)
-                end_time=time.time()
-                XMLParseTime = end_time - start_time
-                print('XML Parse Time = {}'.format(XMLParseTime))
-
-                start_time=time.time()
+           
                 numSubjects, numStudies, numSeries, numImages, numTreeViewItems \
                     = self.objXMLReader.getNumberItemsInTreeView()
 
@@ -49,6 +61,10 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
                 self.progBar = QProgressBar(self)
                 widget.layout().addWidget(self.progBar)
                 widget.layout().setAlignment(Qt.AlignTop)
+
+                numSubjects, numStudies, numSeries, numImages, numTreeViewItems \
+                    = self.objXMLReader.getNumberItemsInTreeView()
+
                 self.progBar.show()
                 self.progBar.setMaximum(numTreeViewItems)
                 self.progBar.setValue(0)
@@ -65,41 +81,28 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
                 subjects = self.objXMLReader.getSubjects()
 
                 for subject in subjects:
-                    subjectName = subject.attrib['id']
-                    subjectBranch = QTreeWidgetItem(self.treeView)
-                    treeWidgetItemCounter += 1
-                    self.progBar.setValue(treeWidgetItemCounter)
-                    subjectBranch.setText(0, "Subject - {}".format(subjectName))
-                    subjectBranch.setFlags(subjectBranch.flags() & ~Qt.ItemIsSelectable)
-                    subjectBranch.setFlags(subjectBranch.flags() | Qt.ItemIsUserCheckable)
-                    subjectBranch.setCheckState(0, Qt.Unchecked)
-                    subjectBranch.setExpanded(True)
-
-                    self.seriesBranchList = []
+                    studyBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                        "Subject", 
+                                                                        subject, 
+                                                                        self.treeView, 
+                                                                        treeWidgetItemCounter)
+                    
                     for study in subject:
-                        studyID = study.attrib['id']
-                        studyBranch = QTreeWidgetItem(subjectBranch)
-                        treeWidgetItemCounter += 1
-                        self.progBar.setValue(treeWidgetItemCounter)
-                        studyBranch.setText(0, "Study - {}".format(studyID))
-                        studyBranch.setFlags(studyBranch.flags() & ~Qt.ItemIsSelectable)
-                        #put a checkbox in front of this branch
-                        studyBranch.setFlags(studyBranch.flags() | Qt.ItemIsUserCheckable)
-                        studyBranch.setCheckState(0, Qt.Unchecked)
-                        studyBranch.setExpanded(True)
-
-                        for series in study:
-                            seriesID = series.attrib['id']
-                            seriesBranch = QTreeWidgetItem(studyBranch)
-                            self.seriesBranchList.append(seriesBranch)
-                            treeWidgetItemCounter += 1
-                            self.progBar.setValue(treeWidgetItemCounter)
-                            seriesBranch.setText(0, "Series - {}".format(seriesID))
-                            seriesBranch.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-                            #put a checkbox in front of this branch
-                            seriesBranch.setFlags(seriesBranch.flags() | Qt.ItemIsUserCheckable)
-                            seriesBranch.setCheckState(0, Qt.Unchecked)
+                        studyBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                            "Study", 
+                                                                            study, 
+                                                                            studyBranch, 
+                                                                            treeWidgetItemCounter)
                        
+                        self.seriesBranchList = []                                                    
+                        for series in study:
+                            seriesBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                                   "Series", 
+                                                                                   series, 
+                                                                                   studyBranch, 
+                                                                                   treeWidgetItemCounter,
+                                                                                   self.seriesBranchList)
+
                             for image in series:
                                 #Extract filename from file path
                                 if image.find('name').text:
@@ -121,16 +124,16 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
                                 imageLeaf.setText(1, imageDate)
                                 imageLeaf.setText(2, imageTime)
                                 imageLeaf.setText(3, imagePath)
-                            seriesBranch.setExpanded(True)
+                           
                 self.treeView.resizeColumnToContents(0)
                 self.treeView.resizeColumnToContents(1)
                 self.treeView.resizeColumnToContents(2)
-                #self.treeView.resizeColumnToContents(3)
                 self.treeView.hideColumn(3)
                 
                 #Now collapse all series branches so as to hide the images
                 for branch in self.seriesBranchList:
-                    branch.setExpanded(False)
+                    if branch !=0:
+                        branch.setExpanded(False)
 
                 self.treeView.customContextMenuRequested.connect(lambda pos: menus.buildContextMenu(self, pos))
                 self.treeView.itemChanged.connect(lambda item: checkChildItems(item))
@@ -139,9 +142,6 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
                 self.treeView.itemDoubleClicked.connect(lambda: menus.viewImage(self))
                 self.treeView.itemClicked.connect(lambda: onTreeViewItemClicked(self, self.treeView.currentItem()))
                 self.treeView.show()
-                end_time=time.time()
-                TreeViewTime = end_time - start_time
-                print('Tree View create Time = {}'.format(TreeViewTime))
                 
                 self.lblLoading.clear()
                 self.progBar.hide()
@@ -153,6 +153,88 @@ def makeDICOMStudiesTreeView(self, XML_File_Path):
             line_number = exception_traceback.tb_lineno
             print('Error in TreeView.makeDICOMStudiesTreeView at line {}: '.format(line_number) + str(e)) 
             logger.error('Error in TreeView.makeDICOMStudiesTreeView at line {}: '.format(line_number) + str(e)) 
+
+
+def refreshDICOMStudiesTreeView(self, newSeriesName = ''):
+        """Uses an XML file that describes a DICOM file structure to build a
+        tree view showing a visual representation of that file structure."""
+        try:
+            logger.info("TreeView.refreshDICOMStudiesTreeView called.")
+            #Load and parse updated XML file
+            self.objXMLReader.parseXMLFile(
+                    self.DICOM_XML_FilePath)
+            self.treeView.clear()
+            self.treeView.setColumnCount(3)
+            self.treeView.setHeaderLabels(["DICOM Files", "Date", "Time", "Path"])
+            treeWidgetItemCounter = 0 
+            subjects = self.objXMLReader.getSubjects()
+            self.seriesBranchList.clear()
+
+            numSubjects, numStudies, numSeries, numImages, numTreeViewItems \
+                    = self.objXMLReader.getNumberItemsInTreeView()
+            self.lblLoading = QLabel('<H4>You are loading {} subject(s) {} study(s), with {} series containing {} images</H4>'
+                 .format(numSubjects, numStudies, numSeries, numImages))
+            self.lblLoading.setWordWrap(True)
+            self.progBar.show()
+            self.progBar.setMaximum(numTreeViewItems)
+            self.progBar.setValue(0)
+
+            for subject in subjects:
+                    studyBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                        "Subject", 
+                                                                        subject, 
+                                                                        self.treeView, 
+                                                                        treeWidgetItemCounter)
+                    
+                    for study in subject:
+                        studyBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                            "Study", 
+                                                                            study, 
+                                                                            studyBranch, 
+                                                                            treeWidgetItemCounter)
+                       
+                        self.seriesBranchList = []                                                    
+                        for series in study:
+                            seriesBranch, treeWidgetItemCounter = createTreeBranch(self, 
+                                                                                   "Series", 
+                                                                                   series, 
+                                                                                   studyBranch, 
+                                                                                   treeWidgetItemCounter,
+                                                                                   self.seriesBranchList)
+
+                            for image in series:
+                                #Extract filename from file path
+                                imageName = os.path.basename(image.find('name').text)
+                                imageDate = image.find('date').text
+                                imageTime = image.find('time').text
+                                imagePath = image.find('name').text
+                                imageLeaf = QTreeWidgetItem(seriesBranch)
+                                imageLeaf.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+                                #put a checkbox in front of each image
+                                imageLeaf.setFlags(imageLeaf.flags() | Qt.ItemIsUserCheckable)
+                                imageLeaf.setCheckState(0, Qt.Unchecked)
+                                imageLeaf.setText(0, ' Image - ' +imageName)
+                                imageLeaf.setText(1, imageDate)
+                                imageLeaf.setText(2, imageTime)
+                                imageLeaf.setText(3, imagePath)
+
+            self.treeView.resizeColumnToContents(0)
+            self.treeView.resizeColumnToContents(1)
+            self.treeView.resizeColumnToContents(2)
+            #Now collapse all series branches so as to hide the images
+            #except the new series branch that has been created
+            expandTreeViewBranch(self, newSeriesName)
+            #If no tree view items are now selected,
+            #disable items in the Tools menu.
+            toggleToolButtons(self)
+            self.lblLoading.clear()
+            self.progBar.hide()
+            self.progBar.reset()
+            self.treeView.hideColumn(3)
+            self.treeView.show()
+        except Exception as e:
+            print('Error in TreeView.refreshDICOMStudiesTreeView: ' + str(e))
+            logger.error('Error in TreeView.refreshDICOMStudiesTreeView: ' + str(e))
 
 
 def checkChildItems(item):
@@ -249,66 +331,6 @@ def expandTreeViewBranch(self, branchText = ''):
             logger.error('Error in TreeView.expandTreeViewBranch: ' + str(e))
 
 
-def refreshDICOMStudiesTreeView(self, newSeriesName = ''):
-        """Uses an XML file that describes a DICOM file structure to build a
-        tree view showing a visual representation of that file structure."""
-        try:
-            logger.info("TreeView.refreshDICOMStudiesTreeView called.")
-            #Load and parse updated XML file
-            self.objXMLReader.parseXMLFile(
-                    self.DICOM_XML_FilePath)
-            self.treeView.clear()
-            self.treeView.setColumnCount(3)
-            self.treeView.setHeaderLabels(["DICOM Files", "Date", "Time", "Path"])
-            studies = self.objXMLReader.getStudies()
-            self.seriesBranchList.clear()
-            for study in studies:
-                studyID = study.attrib['id']
-                studyBranch = QTreeWidgetItem(self.treeView)
-                studyBranch.setText(0, "Study - {}".format(studyID))
-                studyBranch.setFlags(studyBranch.flags() & ~Qt.ItemIsSelectable)
-                studyBranch.setExpanded(True)
-                for series in study:
-                    seriesID = series.attrib['id']
-                    seriesBranch = QTreeWidgetItem(studyBranch)
-                    self.seriesBranchList.append(seriesBranch)
-                    seriesBranch.setText(0, "Series - {}".format(seriesID))
-                     #Uncomment the next 2 lines toput a checkbox in front of this branch
-                    seriesBranch.setFlags(seriesBranch.flags() | Qt.ItemIsUserCheckable)
-                    seriesBranch.setCheckState(0, Qt.Unchecked)
-                    seriesBranch.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-                    seriesBranch.setExpanded(True)
-                    for image in series:
-                        #Extract filename from file path
-                        imageName = os.path.basename(image.find('name').text)
-                        imageDate = image.find('date').text
-                        imageTime = image.find('time').text
-                        imagePath = image.find('name').text
-                        imageLeaf = QTreeWidgetItem(seriesBranch)
-                        imageLeaf.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-                        #Uncomment the next 2 lines to put a checkbox in front of each image
-                        #imageLeaf.setFlags(imageLeaf.flags() | Qt.ItemIsUserCheckable)
-                        #imageLeaf.setCheckState(0, Qt.Unchecked)
-                        imageLeaf.setText(0, ' Image - ' +imageName)
-                        imageLeaf.setText(1, imageDate)
-                        imageLeaf.setText(2, imageTime)
-                        imageLeaf.setText(3, imagePath)
-            self.treeView.resizeColumnToContents(0)
-            self.treeView.resizeColumnToContents(1)
-            self.treeView.resizeColumnToContents(2)
-            #Now collapse all series branches so as to hide the images
-            #except the new series branch that has been created
-            expandTreeViewBranch(self, newSeriesName)
-            #If no tree view items are now selected,
-            #disable items in the Tools menu.
-            toggleToolButtons(self)
-            self.treeView.hideColumn(3)
-            self.treeView.show()
-        except Exception as e:
-            print('Error in TreeView.refreshDICOMStudiesTreeView: ' + str(e))
-            logger.error('Error in TreeView.refreshDICOMStudiesTreeView: ' + str(e))
-
-
 def isAnItemSelected(self):
     """Returns True is an item is selected DICOM
     tree view, else returns False"""
@@ -321,6 +343,7 @@ def isAnItemSelected(self):
     except Exception as e:
         print('Error in isAnItemSelected: ' + str(e))
         logger.error('Error in isAnItemSelected: ' + str(e))
+
 
 def isAnImageSelected(self):
         """Returns True is a single image is selected in the DICOM
@@ -435,9 +458,13 @@ def onTreeViewItemClicked(self, item):
             self.selectedImageName = imageID.strip()
             fullImageID = studyID + ': ' + seriesID + ': '  + imageID
             self.statusBar.showMessage('Image - ' + fullImageID + ' selected.')
+
     except Exception as e:
-            print('Error in TreeView.onTreeViewItemClicked: ' + str(e))
-            logger.error('Error in TreeView.onTreeViewItemClicked: ' + str(e))
+            exception_type, exception_object, exception_traceback = sys.exc_info()
+            #filename = exception_traceback.tb_frame.f_code.co_filename
+            line_number = exception_traceback.tb_lineno
+            print('Error in TreeView.onTreeViewItemClicked at line {}: '.format(line_number) + str(e))
+            logger.error('Error in TreeView.onTreeViewItemClicked at line {}: '.format(line_number) + str(e))
 
 
 def returnSelectedSeries(self):
