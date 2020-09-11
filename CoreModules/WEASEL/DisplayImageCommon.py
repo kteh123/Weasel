Added rotation to pixelArray when reading/writing. getPixelValue bug corrected

CoreModules/WEASEL/DisplayImageCommon.py
@@ -206,14 +206,14 @@ def getPixelValue(pos, imv, pixelArray, lblPixelValue):
             x_i = math.floor(mousePoint.x())
             y_i = math.floor(mousePoint.y()) 
             z_i = imv.currentIndex + 1
-            if (len(np.shape(pixelArray)) == 2) and y_i > 0 and y_i < pixelArray.shape [ 0 ] \
-                and x_i > 0 and x_i < pixelArray.shape [ 1 ]: 
+            if (len(np.shape(pixelArray)) == 2) and y_i > 0 and y_i < pixelArray.shape [ 1 ] \
+                and x_i > 0 and x_i < pixelArray.shape [ 0 ]: 
                 lblPixelValue.setText(
                     "<h4>Pixel Value = {} @ X: {}, Y: {}</h4>"
                 .format (round(pixelArray[ x_i, y_i ], 3), x_i, y_i))
-            elif (len(np.shape(pixelArray)) == 3) and z_i > 0 and z_i < pixelArray.shape [ 0 ] \
+            elif (len(np.shape(pixelArray)) == 3) and z_i > 0 and z_i < pixelArray.shape [ 2 ] \
                 and y_i > 0 and y_i < pixelArray.shape [ 1 ] \
-                and x_i > 0 and x_i < pixelArray.shape [ 2 ]: 
+                and x_i > 0 and x_i < pixelArray.shape [ 0 ]: 
                 lblPixelValue.setText(
                     "<h4>Pixel Value = {} @ X: {}, Y: {}, Z: {}</h4>"
                 .format (round(pixelArray[ z_i, y_i, x_i ], 3), x_i, y_i, z_i))

CoreModules/WEASEL/readDICOM_Image.py
@@ -2,6 +2,8 @@ import os
 import struct
 import numpy as np
 import pydicom
+from scipy.spatial.transform import Rotation
+
 
 def returnPixelArray(imagePath):
     """This method reads the DICOM file in imagePath and returns the Image/Pixel array"""
@@ -169,25 +171,29 @@ def getPixelArray(dataset):
                     slope = float(getattr(dataset.PerFrameFunctionalGroupsSequence[0].PixelValueTransformationSequence[0], 'RescaleSlope', 1)) * np.ones(originalArray.shape)
                     intercept = float(getattr(dataset.PerFrameFunctionalGroupsSequence[0].PixelValueTransformationSequence[0], 'RescaleIntercept', 0)) * np.ones(originalArray.shape)
                     pixelArray = originalArray * slope + intercept
+                    # Ensure that the image will have the correct orientation in the XY plane of the GUI
+                    image_orientation = dataset.PerFrameFunctionalGroupsSequence[0].PlaneOrientationSequence[0].ImageOrientationPatient
+                    pixelArray = rotatePixelArray(pixelArray, image_orientation)
                 else:
                     for index in range(np.shape(originalArray)[0]):
                         sliceArray = np.squeeze(originalArray[index, ...])
                         slope = float(getattr(dataset.PerFrameFunctionalGroupsSequence[index].PixelValueTransformationSequence[0], 'RescaleSlope', 1)) * np.ones(sliceArray.shape)
                         intercept = float(getattr(dataset.PerFrameFunctionalGroupsSequence[index].PixelValueTransformationSequence[0], 'RescaleIntercept', 0)) * np.ones(sliceArray.shape)
                         tempArray = sliceArray * slope.astype(np.float32) + intercept.astype(np.float32)
-                        imageList.append(tempArray)
+                        # Ensure that the image will have the correct orientation in the XY plane of the GUI
+                        image_orientation = dataset.PerFrameFunctionalGroupsSequence[index].PlaneOrientationSequence[0].ImageOrientationPatient
+                        imageList.append(rotatePixelArray(tempArray, image_orientation))
                     pixelArray = np.array(imageList)
                     del sliceArray, tempArray, index
-                if dataset.SharedFunctionalGroupsSequence[0].MRFOVGeometrySequence[0].InPlanePhaseEncodingDirection == "ROW":
-                    pixelArray = np.transpose(pixelArray, axes=(-1,-2))
-                del originalArray
+                del originalArray, imageList
             else:
                 slope = float(getattr(dataset, 'RescaleSlope', 1)) * np.ones(dataset.pixel_array.shape)
                 intercept = float(getattr(dataset, 'RescaleIntercept', 0)) * np.ones(dataset.pixel_array.shape)
                 pixelArray = dataset.pixel_array.astype(np.float32) * slope + intercept
-                if dataset.InPlanePhaseEncodingDirection == "ROW":
-                    pixelArray = np.transpose(pixelArray, axes=(-1,-2))
-            del slope, intercept
+                # Ensure that the image will have the correct orientation in the XY plane of the GUI
+                image_orientation = dataset.ImageOrientationPatient
+                pixelArray = rotatePixelArray(pixelArray, image_orientation)
+            del slope, intercept, image_orientation
             return pixelArray
         else:
             return None
@@ -234,7 +240,28 @@ def getAffineArray(dataset):
             return None
     except Exception as e:
         print('Error in function readDICOM_Image.getAffineArray: ' + str(e))
-    
+
+
+def rotatePixelArray(pixelArray, imageOrientation, invert_rotation=False):
+    """This method reads the pixelArray imageOrientation
+        and returns the rotated pixelArray in the XY plane.
+       Default rotation is counter-clockwise. 
+       If invert_rotation=True, then it's clockwise.
+       In WEASEL, invert_rotation flag is used in saveDICOM_Image.py
+    """
+    try:
+        row_cosine = np.array(imageOrientation[:3])
+        column_cosine = np.array(imageOrientation[3:])
+        rotation_matrix = np.array([row_cosine, column_cosine, np.cross(row_cosine, column_cosine)])
+        rotation_angle = Rotation.from_matrix(rotation_matrix).as_euler('xyz', degrees=True)[0]
+        if invert_rotation: rotation_angle = - rotation_angle
+        number_rotations = int(rotation_angle / 90) if rotation_angle > 0 else int((rotation_angle + 360) / 90)
+        # If number_rotations = 0, it won't rotate the pixelArray
+        rotatedPixelArray = np.rot90(pixelArray, number_rotations)
+        return rotatedPixelArray
+    except Exception as e:
+        print('Error in function readDICOM_Image.rotatePixelArray: ' + str(e))
+
 
 def getColourmap(imagePath):
     """This method reads the DICOM file in imagePath and returns the colourmap if there's any"""