432fa1533bc81982d34ee8e3f5643e5167185bce
S Shillitoe <s.shillitoe@sheffield.ac.uk>
2020-10-02 16:48:40 +0100

    Renamed Packages to SciPackages

---
+++ Developer/SciPackages/tristanAlgorithms.py
@@ -0,0 +1,36 @@
+import numpy as np
+import CoreModules.WEASEL.readDICOM_Image as readDICOM_Image
+
+class TRISTAN():
+    """Package containing algorithms that return deliverables
+    of the IMI-TRISTAN project.
+    
+    Attributes
+    ----------
+    See parameters of __init__ class
+
+    """
+
+    def __init__(self, pixelArray):
+        """Initialise an IMI-TRISTAN class instance.
+        
+
+
+        Parameters
+        ----------
+        pixelArray : np.ndarray of N-dimensions,
+         where axes=0 corresponds to number of timepoints
+         axes=1 corresponds to number of slices
+        """
+
+        self.pixelArray = pixelArray
+
+
+    def MIP(self, sliceList):
+        try:
+            mip = []
+            for index in range(len(sliceList)):
+                mip.append(np.max(self.pixelArray[index, ...], axis=0))
+            return np.array(mip)
+        except Exception as e:
+            print('Error in function TRISTAN.MIP: ' + str(e))
