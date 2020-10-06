86767bca70be212c321f7f6780b37ba1f54aee8b
João Almeida e Sousa <md1jgra@sheffield.ac.uk>
2020-10-06 10:30:20 +0100

    invertImage developer progress

--- Developer/MenuItems/Joao-Development/invertDICOM_Image_Developer.py
+++ Developer/MenuItems/Joao-Development/invertDICOM_Image_Developer.py
@@ -1,11 +1,21 @@
-import Developer.WEASEL.Tools.ToolsFunctions  as toolFunctions
+import Developer.MenuItems.developerToolsModule as tool
 #**************************************************************************
 #Added by third party developer to the template module. 
 #The function containing the image processing algorithm must be given the 
 #generic name, funcAlgorithm
-from Developer.WEASEL.Packages.imagingTools import invertAlgorithm as funcAlgorithm
+from Developer.WEASEL.Packages.imagingTools import invertAlgorithm
+import numpy as np
 FILE_SUFFIX = '_Invert'
 #***************************************************************************
 
 def main(objWeasel):
-    toolFunctions.saveImage(objWeasel, FILE_SUFFIX, funcAlgorithm)
+    # Slice-by-slice approach + overwrite original image
+    # Get all images of the Selected Series
+    imagePathList = tool.getImagePathList(objWeasel)
+    for imagePath in imagePathList:
+        pixelArray = tool.getPixelArrayFromDICOM(imagePath)
+        pixelArray = invertAlgorithm(pixelArray, dtype=np.uint16)
+        # Need to set progress bars here
+        tool.overwritePixelArray(pixelArray, imagePath)
+    # Display series
+    tool.displayImage(objWeasel, imagePathList)
