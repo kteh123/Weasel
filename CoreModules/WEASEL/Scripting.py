96e05ec67caca346e609ddd28a86cbde57cb24b6
plaresmedima <steven.sourbron@googlemail.com>
2021-02-15 00:00:37 +0000

    updated scripting and simplified invert

--- CoreModules/WEASEL/Scripting.py
+++ CoreModules/WEASEL/Scripting.py
@@ -1,4 +1,5 @@
 import CoreModules.WEASEL.TreeView as treeView
+import CoreModules.WEASEL.MessageWindow as messageWindow
 from CoreModules.DeveloperTools import Image
 
 class Scripting():
@@ -12,3 +13,25 @@ class Scripting():
         for images in imagesTreeViewList:
             imagesList.append(Image.fromTreeView(self, images))
         return imagesList
+
+    def progressBar(self, maxNumber=1, index=0, msg="Iteration Number {}", title="Progress Bar"):
+        """
+        Displays a Progress Bar with the unit set in "index".
+        """
+        messageWindow.displayMessageSubWindow(self, ("<H4>" + msg + "</H4>").format(index), title)
+        messageWindow.setMsgWindowProgBarMaxValue(self, maxNumber)
+        messageWindow.setMsgWindowProgBarValue(self, index)
+
+    def closeProgressBar(self):
+        """
+        Closes the Progress Bar.
+        """
+        messageWindow.hideProgressBar(self)
+        messageWindow.closeMessageSubWindow(self)
+
+    def DisplayImages(self, imageList):
+        """
+        Displays all images in a list of images.
+        """
+        if len(imageList) == 0: return
+        Image.DisplayImages(imageList)
--- Pipelines/Template_InvertPixelValues.py
+++ Pipelines/Template_InvertPixelValues.py
@@ -1,23 +1,10 @@
-from CoreModules.DeveloperTools import UserInterfaceTools, Image
-
-# Invert all pixel values and overwrite existing images
-# The calculation is done slice-by-slice with an update through a progress bar
-
 def main(Weasel):
-    ui = UserInterfaceTools(Weasel)
-    # Get all images in the Checkboxes
-    # imageList = ui.getCheckedImages()
-    imageList = Weasel.images()
-    if len(imageList) == 0: return # Exit function if no images are checked
-    index_bar = 0
-    for image in imageList:
-        # Progress Bar
-        index_bar = ui.progressBar(maxNumber=len(imageList), index=index_bar, msg="Inverting and overwriting image {}", title="Invert pixel values ")
-        # Invert Pixel Array
-        pixelArray = - image.PixelArray
-        # Overwrite - write() checks if file already exists so it writes new or overwrite
-        image.write(pixelArray)
-    # Close the progress bar    
-    ui.closeMessageWindow()
-    # Display all checked images
-    Image.DisplayImages(imageList)
+    images = Weasel.images()
+    if len(images) == 0: return 
+    for i, image in enumerate(images):
+        Weasel.progressBar(maxNumber=len(images), index=i, msg="Inverting image {}", title="Invert pixel values ")
+        image.write(-image.PixelArray) 
+    Weasel.closeProgressBar()
+    Weasel.DisplayImages(images)
+    # better would be
+    # images.display()
