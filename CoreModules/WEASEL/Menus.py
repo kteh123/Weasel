8221eb840cb24239320f299b4aca2eaf0e79585f
Steve Shillitoe <s.shillitoe@sheffield.ac.uk>
2021-02-18 18:28:03 +0000

    Menu items only enabled when a series or image is selected. Context menu only displayed when a series or image is selected.

--- CoreModules/WEASEL/Menus.py
+++ CoreModules/WEASEL/Menus.py
@@ -82,31 +82,6 @@ def buildUserDefinedToolsMenuItem(self, topMenu, item):
         print('Error in function Menus.buildUserDefinedToolsMenuItem at line number {} when {}: '.format(line_number, item.find('label').text) + str(e))
 
 
-#def closeAllImageWindows(self):
-#        """Closes all the sub windows in the MDI except for
-#        the sub window displaying the DICOM file tree view"""
-#        logger.info("Menus closeAllImageWindows called")
-#        for subWin in self.mdiArea.subWindowList():
-#            if subWin.objectName() == 'tree_view':
-#                continue
-#            subWin.close()
-
-
-#def tileAllSubWindows(self):
-#    logger.info("Menus.tileAllSubWindow called")
-#    height, width = self.getMDIAreaDimensions()
-#    for subWin in self.mdiArea.subWindowList():
-#        if subWin.objectName() == 'tree_view':
-#            subWin.setGeometry(0, 0, width * 0.4, height)
-#        elif subWin.objectName() == 'Binary_Operation':
-#            subWin.setGeometry(0,0,width*0.5,height*0.5)
-#        elif subWin.objectName() == 'metaData_Window':
-#            subWin.setGeometry(width * 0.4,0,width*0.6,height)
-#        elif subWin.objectName() == 'image_viewer':
-#            subWin.setGeometry(width * 0.4,0,width*0.3,height*0.5)
-#        self.mdiArea.tileSubWindows()
-
-
 def buildContextMenuItem(context, item, self):
     menuItem = QAction(item.find('label').text, self)
     menuItem.setEnabled(True)
@@ -135,7 +110,8 @@ def buildContextMenuItem(context, item, self):
     
 
 def displayContextMenu(self, pos):
-    self.context.exec_(self.treeView.mapToGlobal(pos))
+    if not treeView.isASubjectSelected(self) and not treeView.isAStudySelected(self):
+        self.context.exec_(self.treeView.mapToGlobal(pos))
 
 
 def _actionHovered(action):
