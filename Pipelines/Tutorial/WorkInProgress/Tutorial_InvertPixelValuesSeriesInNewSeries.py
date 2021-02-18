d9197e11406fcb6098e0424d4b8990a728744f61
plaresmedima <steven.sourbron@googlemail.com>
2021-02-18 22:40:07 +0000

    Update Tutorial_InvertPixelValuesSeriesInNewSeries.py

--- Pipelines/Tutorial/WorkInProgress/Tutorial_InvertPixelValuesSeriesInNewSeries.py
+++ Pipelines/Tutorial/WorkInProgress/Tutorial_InvertPixelValuesSeriesInNewSeries.py
@@ -6,13 +6,13 @@
 #***************************************************************************
 
 def main(Weasel):
-    List = Weasel.Series()    # get the list of series checked by the user
-    if List.Empty(): return   # if none are checked then do nothing
-    for i, Series in List.Enumerate():      # Loop over Series in the list and display a progress Bar
+    List = Weasel.Series()      # get the list of series checked by the user
+    if List.Empty(): return     # if none are checked then do nothing
+    for i, Series in List.Enumerate():              # Loop over Series in the list and display a progress Bar
         Weasel.ProgressBar(max=List.Count(), index=i+1, msg="Inverting series {}", title="Invert pixel values ")
-        newSeries = Series.new(series_name="Invert_"+str(Series.seriesID)) # Derive a new series
-        newSeries.write(-Series.PixelArray) # Write the results in the new series
-        newSeries.Display()     # Display the new series
+        newSeries = Series.new(suffix="_Invert")    # Derive a new series
+        newSeries.write(-Series.PixelArray)         # Write the results in the new series
+    newSeries.Display()     # Display the new series
     Weasel.CloseProgressBar()   # Close the progress bar
     Weasel.Refresh(new_series_name=newSeries.seriesID) # Refresh weasel
     
