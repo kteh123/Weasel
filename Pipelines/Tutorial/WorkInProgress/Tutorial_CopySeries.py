a57c3d1e9f377eb72a98dbcb6d3f7ef0b6339c2c
plaresmedima <steven.sourbron@googlemail.com>
2021-02-19 00:44:56 +0000

    Update Tutorial_CopySeries.py

--- Pipelines/Tutorial/WorkInProgress/Tutorial_CopySeries.py
+++ Pipelines/Tutorial/WorkInProgress/Tutorial_CopySeries.py
@@ -9,7 +9,6 @@ def main(Weasel):
     List = Weasel.Series()                  # get the list of series checked by the user
     for i, Series in List.Enumerate():      # Loop over Series in the list and display a progress Bar
         Weasel.ProgressBar(max=List.Count(), index=i+1, msg="Copying series {}")
-        newSeries = Series.Copy()
-        newSeries.Display()     # Display the new series
-    Weasel.Refresh()            # Refresh weasel
+        Series.Copy().Display()     # Copy and Display the new series  
+    Weasel.Refresh()                # Refresh weasel
     
