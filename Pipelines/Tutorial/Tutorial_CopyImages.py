9e7f64e2bd4d6c9ad4b483e84a827623fef26e91
plaresmedima <steven.sourbron@googlemail.com>
2021-03-09 22:44:54 +0000

    Copy and merge updates

--- Pipelines/Tutorial/Tutorial_CopyImages.py
+++ Pipelines/Tutorial/Tutorial_CopyImages.py
@@ -4,10 +4,11 @@
 #***************************************************************************
 
 def main(weasel):
+
     list_of_images = weasel.images()                 # get the list of series checked by the user
     for i, image in list_of_images.enumerate():      # Loop over Series in the list and display a progress Bar
         weasel.progress_bar(max=list_of_images.length(), index=i+1, msg="Copying images {}")
-        image.copy()                # Copy the new image 
+        image.copy()                # Copy the image 
     weasel.refresh()                # Refresh weasel
 
     
