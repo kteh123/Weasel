590cd470182c6f202be0afeb9aa618646ac92920
plaresmedima <steven.sourbron@googlemail.com>
2021-03-15 21:16:52 +0000

    Update Tutorial_Delete.py

--- Pipelines/Tutorial/Tutorial_Delete.py
+++ Pipelines/Tutorial/Tutorial_Delete.py
@@ -9,7 +9,7 @@
 def main(weasel):
     list_of_images = weasel.images()                 # get the list of images checked by the user
     for i, image in enumerate(list_of_images):      # Loop over Images in the list and display a progress Bar
-        weasel.progress_bar(max=length(list_of_images), index=i+1, msg="Deleting images {}")
+        weasel.progress_bar(max=len(list_of_images), index=i+1, msg="Deleting images {}")
         image.delete()                     # Delete the image  
     weasel.refresh()                
     
