15c700346f3cc9cabe2fa5c53e5c2eb607b3ec81
plaresmedima <steven.sourbron@googlemail.com>
2021-03-22 14:20:25 +0000

    Scripting updates

---
+++ Scripting/Folder_to_DicomDataFrame.py
@@ -0,0 +1,24 @@
+import os
+
+from Scripting.Files_to_DicomDataFrame import Files_to_DicomDataFrame
+
+def Folder_to_DicomDataFrame(folder, weasel):
+    """Read all DICOM files in a folder and return a pandas dataframe
+    where each row corresponds to a valid DICOM file and 
+    the columns present values of tags requested.
+    """
+    files = [item.path for item in scan_tree(folder) if item.is_file()]
+    tags = [
+        'PatientID', 'StudyInstanceUID', 'SeriesInstanceUID', 'SOPInstanceUID', 
+        'PatientName', 'StudyDescription', 'SeriesDescription', 'SeriesNumber', 'InstanceNumber', 
+        'StudyDate', 'SeriesDate', 'AcquisitionDate',
+        'StudyTime', 'SeriesTime', 'AcquisitionTime' ]
+    return Files_to_DicomDataFrame(files, tags, weasel)
+
+def scan_tree(folder):
+    """Recursively yield DirEntry objects for given directory."""
+    for entry in os.scandir(folder):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scan_tree(entry.path)
+        else:
+            yield entry
--- Scripting/Scripting.py
+++ Scripting/Scripting.py
@@ -1,12 +1,12 @@
 import os
-from PyQt5.QtWidgets import (QMessageBox, QFileDialog)
+
 import CoreModules.WEASEL.TreeView as treeView
-import CoreModules.WEASEL.MessageWindow as messageWindow
 from CoreModules.DeveloperTools import UserInterfaceTools
 from CoreModules.DeveloperTools import Study
 from CoreModules.DeveloperTools import Series
 from CoreModules.DeveloperTools import Image
-from CoreModules.UserInput import UserInput
+from Scripting.UserInput import UserInput
+from Scripting.WslDisplay import WslDisplay
 
 class ListOfDicomObjects(list):
     """
@@ -93,8 +93,7 @@ class StudyList(ListOfDicomObjects):
     A class containing a list of class Study. 
     """
 
-
-class Pipelines(UserInput):
+class Pipelines(UserInput, WslDisplay):
     """
     A class for accessing GUI elements from within a pipeline script. 
     """
@@ -137,86 +136,4 @@ class Pipelines(UserInput):
                 studyList.append(Study.fromTreeView(self, study))
         return StudyList(studyList)
  
-    def progress_bar(self, max=1, index=0, msg="Iteration Number {}", title="Progress Bar"):
-        """
-        Displays a progress bar with the unit set in "index".
-        Note: launching a new progress bar at each iteration costs time, so this
-        should only be used in iterations where the progress bar is updated infrequently
-        For iterations with frequent updates, use progress_bar outside the iteration
-        and then update_progress_bar inside the iteration
-        """
-        messageWindow.displayMessageSubWindow(self, ("<H4>" + msg + "</H4>").format(index), title)
-        messageWindow.setMsgWindowProgBarMaxValue(self, max)
-        messageWindow.setMsgWindowProgBarValue(self, index)
-
-    def update_progress_bar(self, index=0):
-        """
-        Updates the progress bar with a new index.
-        """
-        messageWindow.setMsgWindowProgBarValue(self, index)
-
-    def close_progress_bar(self):
-        """
-        Closes the Progress Bar.
-        """
-        messageWindow.hideProgressBar(self)
-        messageWindow.closeMessageSubWindow(self)
-
-    def message(self, msg="Hello world!", title="Message window"):
-        """
-        Displays a window in the User Interface with the title in "title" and
-        with the message in "msg". 
-        """
-        messageWindow.displayMessageSubWindow(self, "<H4>" + msg + "</H4>", title)
-
-    def close_message(self):
-        """
-        Closes the message window 
-        """
-        self.msgSubWindow.close()
-
-    def information(self, msg="Are you OK today?", title="Message window"):
-        """
-        Displays an information window in the User Interface with the title in "title" and
-        with the message in "msg". The user has to click "OK" in order to continue using the interface.
-        """
-        QMessageBox.information(self, title, msg)
-
-    def question(self, msg="Shall we carry on?", title="Message Window Title"):
-        """
-        Displays a question window in the User Interface with the title in "title" and
-        with the question in "msg". 
-        The user has to click either "OK" or "Cancel" in order to continue using the interface.
-        It returns 0 if reply is "Cancel" and 1 if reply is "OK".
-        """
-        buttonReply = QMessageBox.question(self, title, msg, 
-                      QMessageBox.Ok | QMessageBox.Cancel, QMessageBox.Cancel)
-        if buttonReply == QMessageBox.Ok:
-            return 1
-        else:
-            return 0
-
-    def refresh(self, new_series_name=None):
-        """
-        Refreshes the Weasel display.
-        """
-        self.close_progress_bar()
-        ui = UserInterfaceTools(self)
-        ui.refreshWeasel(new_series_name=new_series_name)
-
-    def close_all_windows(self):
-        """
-        Closes all open windows.
-        """
-        self.mdiArea.closeAllSubWindows()
-
-    def folder(self, msg='Please select a folder'):
-        """
-        Ask the user to select a folder
-        """
-        return QFileDialog.getExistingDirectory(self,
-            msg, 
-            self.weaselDataFolder, 
-            QFileDialog.ShowDirsOnly
-        )
 
