b9ca8aa377f95926d5f7e0ae9403d3c0522d47c1
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2021-04-19 14:30:50 +0100

    Python script that generates binary file for each of the 3 OS added (Windows, Mac and Linux). Issue #38

---
+++ compile_and_generate_binary.py
@@ -0,0 +1,47 @@
+import os
+from sys import platform
+import venv
+
+print("Creating Python Virtual Environment...")
+venv_dir = os.path.join(os.getcwd(), "venv")
+os.makedirs(venv_dir, exist_ok=True)
+venv.create(venv_dir, with_pip=True)
+
+print("Activating the Python Virtual Environment created...")
+# Windows
+if platform == "win32" or platform == "win64" or os.name == 'nt':
+	#activation_command = "venv\\Scripts\\activate.bat"
+	activation_command = str(venv_dir) + "\\Scripts\\activate"
+# MacOS and Linux
+else:
+	#activation_command = "venv/bin/activate"
+	activation_command = ". " + venv_dir + "/bin/activate"
+
+print("Installing Python packages in the Virtual Environment...")
+os.system(activation_command + ' && pip install -e .')
+print("Cleaning up installation files...")
+os.system(activation_command + ' && python setup.py clean')
+
+print("Starting compilation...")
+os.system(activation_command + ' && pyinstaller --hidden-import requests --hidden-import xnat --onefile Weasel.py')
+#os.system('pyinstaller --hidden-import requests --hidden-import xnat --exclude-module External --onefile Weasel.py')
+
+print("Cleaning up compilation files...")
+# Windows
+if platform == "win32" or platform == "win64" or os.name == 'nt':
+	os.system('move dist\* .')
+	os.system('rmdir build /S /Q')
+	os.system('rmdir dist /S /Q')
+	os.system('del Weasel.spec')
+	print("Deleting the created Python Virtual Environment for the process...")
+	os.system('rmdir venv /S /Q')
+# MacOS and Linux
+else:
+	os.system('rm -rf Weasel.egg-info')
+	os.system('mv dist/* .')
+	os.system('rm -rf build/ dist/')
+	os.system('rm Weasel.spec')
+	print("Deleting the created Python Virtual Environment for the process...")
+	os.system('rm -r venv/')
+
+print("Binary file successfully created and saved in the Weasel repository!")
