e67131fda88860583260ebb550661440b8bdc816
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2021-05-13 17:25:28 +0100

    updated comments

--- CoreModules/WEASEL/SeriesSelector.py
+++ CoreModules/WEASEL/SeriesSelector.py
@@ -7,13 +7,26 @@ logger = logging.getLogger(__name__)
 __author__ = "Steve Shillitoe"
 
 class SeriesSelector:
-    """description of class"""
-    def __init__(self, title, pointerToWeasel, mask=False):
+    """This class creates three linked cascading lists in a 
+    group box with the title groupBoxTitle.
+    Together with an adjacent label, the lists are arranged 
+    vertically in a column. 
+    The top list allows the user to select the subject. 
+    The subject selection dynamically determines the contents 
+    of the middle study list. Only studies belonging to the 
+    selected subject are displayed. 
+    The study selected dynamically determines the contents 
+    of the bottom series list.  Only series belonging to the
+    selected study are displayed. 
+
+    The property selectedSeriesData returns the names of selected
+    subject, study and series in that order. 
+    """
+    def __init__(self, pointerToWeasel, groupBoxTitle=""):
         try:
             logger.info("SeriesSelect object created")
-            self.title = title
+            self.title = groupBoxTitle
             self.weasel = pointerToWeasel
-            self.mask = mask
         except Exception as e:
             print('Error in class SeriesSelector.__init__: ' + str(e))
             logger.error('Error in class SeriesSelector.__init__: ' + str(e))
@@ -52,12 +65,8 @@ class SeriesSelector:
             studyID = studyList[0]
             self.listStudies.addItems(studyList)
             QApplication.processEvents()
-            if self.mask:
-                maskList = self.weasel.objXMLReader.getMaskList(subjectID, studyID)
-                self.listSeries.addItems(maskList)
-            else:
-                seriesList = self.weasel.objXMLReader.getNonMaskSeriesList(subjectID, studyID)
-                self.listSeries.addItems(seriesList)
+            seriesList = self.weasel.objXMLReader.getSeriesList(subjectID, studyID)
+            self.listSeries.addItems(seriesList)
 
         except Exception as e:
             print('Error in SeriesSelector.initialiseLists: ' + str(e))
@@ -67,10 +76,7 @@ class SeriesSelector:
     def setUpEvents(self):
         try:
             self.listSubjects.currentIndexChanged.connect(self.updateStudy)
-            if self.mask:
-                self.listStudies.currentIndexChanged.connect(self.updateMasks)
-            else:
-                self.listStudies.currentIndexChanged.connect(self.updateSeries)
+            self.listStudies.currentIndexChanged.connect(self.updateSeries)
         except Exception as e:
             print('Error in SeriesSelector.setUpEvents: ' + str(e))
             logger.exception('Error in SeriesSelector.setUpEvents: ' + str(e))
@@ -86,18 +92,10 @@ class SeriesSelector:
     def updateSeries(self):
         subjectID = self.listSubjects.currentText()
         studyID = self.listStudies.currentText()
-        seriesList = self.weasel.objXMLReader.getNonMaskSeriesList(subjectID, studyID)
+        seriesList = self.weasel.objXMLReader.getSeriesList(subjectID, studyID)
         self.listSeries.clear()
         self.listSeries.addItems(seriesList)
-        
-
-    def updateMasks(self):
-        subjectID = self.listSubjects.currentText()
-        studyID = self.listStudies.currentText()
-        maskList = self.weasel.objXMLReader.getMaskList(subjectID, studyID)
-        self.listSeries.clear()
-        self.listSeries.addItems(maskList)
-        
+             
 
     @property
     def selectedSeriesData(self):
