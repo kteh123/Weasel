a55308646bb0c60a03a4e90f3fb40222cd373169
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2021-10-13 18:30:14 +0100

    Python Virtualenv in reference manual script

--- create_weasel_manual.py
+++ create_weasel_manual.py
@@ -1,5 +1,6 @@
 import os, shutil
 from sys import platform
+import venv
 
 def CopyTree(src, dst, symlinks=False, ignore=None):
     if not os.path.exists(dst):
@@ -44,9 +45,25 @@ CopyTree(menus_folder, os.path.join(weasel_copy_folder, "Menus"))
 CopyTree(pipelines_folder, os.path.join(weasel_copy_folder, "Pipelines"))
 shutil.copyfile(weasel_file, os.path.join(weasel_copy_folder, "Weasel.py"))
 
+print("Creating Python Virtual Environment for the occasion...")
+venv_dir = os.path.join(os.getcwd(), "venv")
+os.makedirs(venv_dir, exist_ok=True)
+venv.create(venv_dir, with_pip=True)
+
+print("Activating the Python Virtual Environment created...")
+# Windows
+if platform == "win32" or platform == "win64" or os.name == 'nt':
+	activation_command = str(venv_dir) + "\\Scripts\\activate"
+# MacOS and Linux
+else:
+	activation_command = ". " + venv_dir + "/bin/activate"
+
+print("Installing Python packages in the Virtual Environment...")
+os.system(activation_command + ' && pip install -e .')
+
 print("Creating WEASEL manual using pdoc3...")
 doc_command = "pdoc3 --html --force --output-dir " + str(docs_folder) + " " + str(weasel_copy_folder)
-os.system(doc_command)
+os.system(activation_command + ' && ' + doc_command)
 
 print("Moving documentation files to the 'Manual' folder...")
 shutil.rmtree(temporary_folder)
