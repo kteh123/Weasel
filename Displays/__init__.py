793c9dcc039ba39826ab0d01bfeb9af8922d42b2
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2021-12-04 15:31:01 +0000

    Creating new executables to test the DrawROI

--- Displays/__init__.py
+++ Displays/__init__.py
@@ -1,8 +1,1 @@
 """Test docstring pdoc3 Display"""
-
-import ImageViewers.ImageViewer
-import ImageViewers.ComponentsUI
-import ImageViewers.ComponentsUI.FreeHandROI
-import ImageViewers.ComponentsUI.FreeHandROI.GraphicsView
-import ImageViewers.ComponentsUI.FreeHandROI.Resources
-import ImageViewers.ImageViewerROI
