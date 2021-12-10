551a47f412b70a765e6cfdb090fd476db094e369
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2021-12-10 19:28:33 +0000

    Update __init__.py

--- Displays/ImageViewers/__init__.py
+++ Displays/ImageViewers/__init__.py
@@ -1,1 +1,5 @@
-
+"""
+Contains modules that support the display of series of DICOM images and individual
+DICOM images (ImageViewer.py) 
+and with the facility to draw an ROI on an image (ImageViewerROI.py)
+"""
