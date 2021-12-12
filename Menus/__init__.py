aba78cf740c461de8d6ab215da2fafa0ce795268
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2021-12-12 23:41:39 +0000

    Create __init__.py

---
+++ Menus/__init__.py
@@ -0,0 +1,22 @@
+"""
+Modules that contain the definition of the items forming 
+a menu in the Weasel GUI menu bar.
+
+Demo.py - Contains the definition of the items in the Demo menu.
+Edit.py - Contains the definition of the items in the Edit menu,
+            used to edit images and series.
+File.py - Contains the definition of the items in the File menu, 
+            used to open DICOM folders.
+GettingStarted_Developers.py - Contains the definition of 
+            several top-level menus in the Weasel GUI menu bar.
+GettingStarted_EndUsers.py - Contains the definition of 
+            several top-level menus in the Weasel GUI menu bar.
+Help.py - Contains the definition of the items in the Help menu. 
+Menus.xml - Contains the definition in XML format of 
+            several top-level menus in the Weasel GUI menu bar.
+Tutorial.py - Contains the definition of the items in the Tutorial menu. 
+View.py - Contains the definition of the items in the View menu, 
+            used to view images and series. 
+XNAT.py - Contains the definition of the items in the XNAT menu, 
+            used to upload and download DICOM data from XNAT. 
+"""
