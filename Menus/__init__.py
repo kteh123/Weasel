9cda7539fa67171d244f2c713a7567f3ed49e630
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2021-12-15 14:27:24 +0000

    Update __init__.py
    Rewrote the menus doc string

--- Menus/__init__.py
+++ Menus/__init__.py
@@ -1,29 +1,75 @@
 """
-Modules that contain the definition of the items forming 
-a menu in the Weasel GUI menu bar.
+This folder contains Python and XML files that define the menus in the
+menu bar at the top of the Weasel GUI. 
 
-Edit.py - Contains the definition of the items in the Edit menu,
-            used to edit images and series.
+The following files contain the definition of standard Weasel menus 
+that allow the user to access key Weasel functionality
+(see individual files for more details):
 
-File.py - Contains the definition of the items in the File menu, 
-            used to open DICOM folders.
+File.py - Definition of the File menu
+Edit.py - Definition of the Edit menu
+Help.py - Definition of the Help menu
+View.py - Definition of the View menu
+XNAT.py - Definition of the XNAT menu
+Tutorial.py - Definition the Tutorial menu. 
 
-GettingStarted_Developers.py - Contains the definitions of 
-            several top-level menus in the Weasel GUI menu bar.
+The above files can be combined in an overarching menu file 
+that defines all the menus in the menu bar. The following files
+are examples of this:
+ 
+GettingStarted_Developers.py
+GettingStarted_EndUsers.py
 
-GettingStarted_EndUsers.py - Contains the definition of 
-            several top-level menus in the Weasel GUI menu bar.
+A developer may create their own bespoke menu file similiar to these
+2 files including their own menus as well as including any or all
+of the above standard Weasel menus. To do this a developer may wish
+to copy, rename and edit one of the above 2 files.
 
-Help.py - Contains the definition of the items in the Help menu. 
+Every menu file must include a function called main 
+with the argument weasel; thus,
 
-Menus.xml - Contains the definition in XML format of 
-            several top-level menus in the Weasel GUI menu bar.
+def main(weasel):
 
-Tutorial.py - Contains the definition of the items in the Tutorial menu. 
+within the main function, the following function calls will include 
+the standard Weasel menus,
 
-View.py - Contains the definition of the items in the View menu, 
-            used to view images and series. 
+    weasel.menu_file()  #file menu
+    weasel.menu_view()  #view menu
+    weasel.menu_edit()  #edit menu
+    weasel.menu_xnat()  #XNAT menu
+    weasel.menu_help()  #help menu
 
-XNAT.py - Contains the definition of the items in the XNAT menu, 
-            used to upload and download DICOM data from XNAT. 
+The following function call will create a new menu called "My Menu" 
+in the menu bar,
+
+myMenu = weasel.menu("My Menu")
+
+Items in this menu are created with the following function call,
+    myMenu.item(
+        label = 'My Menu Item',
+        tooltip = 'My menu item allows the user to .......',
+        pipeline = 'My_Module_Name')
+
+where label - Menu item name in the drop down menu.
+      tooltip - Menu item tooltip displayed when the mouse pointer 
+                hovers over the menu item.
+      pipeline - The name of the Python module without the .py extension
+                that contains the functionality invoked by the this menu item.
+                When this menu item is selected, it expects to call a function 
+                called main in this module.  Therefore, this module must 
+                contain a function with the definition 
+                    def main(weasel):
+                that contains the logic underpinning the functionality of this
+                menu item.
+
+The following function call will draw a horizontal line in the menu 
+to separate different parts
+    menu.separator()
+
+The name of the new menu file must be included in the config.xml file, that is 
+in the root of the Weasel source code, enclosed in <menu_config_file> tags thus
+
+    <menu_config_file>GettingStarted_EndUsers.py</menu_config_file>
+
+Menus.xml is an xml definition of several menus in the menu bar of Weasel
 """
