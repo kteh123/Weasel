554a150c106db26437554db772b6edb66aa6d816
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2022-02-09 14:50:14 +0000

    Fix to incorrect word wrapping of the pixel value label

--- Displays/ImageViewers/ComponentsUI/PixelValueLabel.py
+++ Displays/ImageViewers/ComponentsUI/PixelValueLabel.py
@@ -10,13 +10,31 @@ class PixelValueComponent:
         self.pixelValueLayout = QHBoxLayout()
         self.pixelValueLayout.setContentsMargins(0, 0, 0, 0)
         self.pixelValueLayout.setSpacing(0)
-
+        self.labelWidth = 0
         self.lblPixelValue = QLabel()
         self.lblPixelValue.setWordWrap(True)
         self.lblPixelValue.setMargin(0)
         self.lblPixelValue.setStyleSheet(
             "color : red; padding-left:0; margin-left:0; padding-right:0; margin-right:0; font-size:8pt;")
-        self.pixelValueLayout.addWidget(self.lblPixelValue, alignment=Qt.AlignCenter)
+        self.pixelValueLayout.addWidget(self.lblPixelValue,
+                                       alignment=Qt.AlignCenter)
+
+
+    def setLabelWidth(self):
+        """
+        This function sets the width of the label to the width
+        of its container layout.  
+
+        Setting the width of the label to that of its container
+        layout means the label word wraps only when the length
+        of the text it contains exceeds the width of the label.
+        """
+        layoutWidth = self.pixelValueLayout.contentsRect().width()
+        if self.labelWidth != layoutWidth:
+            #only update the label width if it has changed
+            self.labelWidth = layoutWidth
+            self.lblPixelValue.setFixedWidth(layoutWidth)
+        
 
     def getLayout(self):
         """
